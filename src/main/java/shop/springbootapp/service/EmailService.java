aac37ad895c6202e82afb804041f49fffd57c1b1
BSGanchev <BSGanchev@abv.bg>
2023-11-20 12:16:22 +0200

    rollback

--- src/main/java/shop/springbootapp/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package shop.springbootapp.service;
-
-import jakarta.mail.MessagingException;
-
-public interface EmailService {
-    void sendMail(String recipientAddress, String text, String subject, boolean isHtml) throws MessagingException;
-
-}
--- src/main/java/shop/springbootapp/service/UserActivationService.java
+++
@@ -1,9 +0,0 @@
-package shop.springbootapp.service;
-
-import jakarta.mail.MessagingException;
-import shop.springbootapp.model.events.RegistrationEvent;
-
-public interface UserActivationService {
-
-    void userRegister(RegistrationEvent event) throws MessagingException;
-}
